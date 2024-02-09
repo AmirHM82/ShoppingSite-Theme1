b0f15185e595455f16bb23876a0bab394a360606
AmirHM82 <ami00mou@gmail.com>
2024-02-10 01:23:22 +0330

    A simple change on address bar variables

--- js/Addressbar.js
+++ js/Addressbar.js
@@ -5,15 +5,17 @@ var parent = document.getElementsByClassName('address-bar')[0];
 
 let href = window.location.origin;
 
+let span = null;
+let a = null;
 let counter = 0;
 for (let p of pathContent) {
     counter += 1;
-    let span = document.createElement('span');
+    span = document.createElement('span');
     span.classList.add('address-bar-space');
     span.innerText = '/';
     parent.appendChild(span);
 
-    let a = document.createElement('a');
+    a = document.createElement('a');
     a.classList.add('address-bar-item');
     a.innerText = p.replace('.html', '');
     if (pathContent.length != counter) {
