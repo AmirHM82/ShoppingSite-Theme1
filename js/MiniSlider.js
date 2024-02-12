0dfb22a66e49893046cc90466189d2032c333c00
AmirHM82 <ami00mou@gmail.com>
2024-02-12 20:40:21 +0330

    pagination scripts are writen, related bugs are resolved

--- js/MiniSlider.js
+++ js/MiniSlider.js
@@ -3,6 +3,13 @@ window.setInterval(changeSlider, 2000);
 let turn = 0;
 function changeSlider() {
     var imgs = document.getElementsByClassName('slider-image-wrap');
+    var icons = document.getElementsByClassName('pagination-icon');
+
+    if (icons[turn].classList.contains('fa-solid')) {
+        icons[turn].classList.remove('fa-solid');
+        icons[turn].classList.add('fa-regular');
+    }
+
     if (imgs[turn].classList.contains('slider-visible')) {
         imgs[turn].classList.remove('slider-visible');
         imgs[turn].classList.add('slider-hidden');
@@ -12,6 +19,11 @@ function changeSlider() {
 
     if (turn > (imgs.length - 1)) {
         turn = 0;
+    }
+
+    if (icons[turn].classList.contains('fa-regular')) {
+        icons[turn].classList.remove('fa-regular');
+        icons[turn].classList.add('fa-solid');
     }
 
     if (imgs[turn].classList.contains('slider-hidden')) {
--- js/Slider.js
+++ js/Slider.js
@@ -4,9 +4,29 @@ move(0);
 
 function move(n) {
     var slides = document.getElementsByClassName('slider-image-wrap');
-    if (sliderPos >= 0 && sliderPos < slides.length && sliderPos + n >= 0 && sliderPos + n < slides.length) {
-        slides[sliderPos].style.width = '0';
-        sliderPos += n;
-        slides[sliderPos].style.width = '100%';
+    var icons = document.getElementsByClassName('pagination-icon');
+
+    if (icons[sliderPos].classList.contains('fa-solid')) {
+        icons[sliderPos].classList.remove('fa-solid');
+        icons[sliderPos].classList.add('fa-regular');
     }
+
+    slides[sliderPos].style.width = '0';
+
+    sliderPos += n;
+
+    if (sliderPos > (slides.length - 1)) {
+        sliderPos = 0;
+    }
+
+    if (sliderPos < 0) {
+        sliderPos = (slides.length - 1);
+    }
+
+    if (icons[sliderPos].classList.contains('fa-regular')) {
+        icons[sliderPos].classList.remove('fa-regular');
+        icons[sliderPos].classList.add('fa-solid');
+    }
+
+    slides[sliderPos].style.width = '100%';
 }
