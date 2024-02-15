f4676f324871368780ea87c1c09184c4f9109a1f
AmirHM82 <ami00mou@gmail.com>
2024-02-15 18:17:00 +0330

    - Change of plans: search section is in the products menu it self - fixed some bugs with scripts - Add some features alongside search section - Fixed pagination counter

--- js/ProductPagination.js
+++ js/ProductPagination.js
@@ -2,6 +2,8 @@ const lastPath = window.location.pathname.split('/').pop();
 
 const urlParams = new URLSearchParams(window.location.search);
 
+const searchParam = urlParams.get('search');
+
 const maxPageStr = urlParams.get('MaxPage');
 let maxPage = 2;
 if (maxPageStr != null) {
@@ -14,15 +16,31 @@ if (pageStr != null) {
     page = parseInt(pageStr);
 }
 
+var counterElement = document.getElementById('page-counter');
+counterElement.innerHTML = 'صفحه' + page + '/' + maxPage;
+
 var leftBtn = document.getElementById('leftBtn');
 var rightBtn = document.getElementById('rightBtn');
 
 let nextPage = page + 1;
 let previousPage = page - 1;
 
+let href = './' + lastPath;
+
+if (searchParam != null) {
+    href += '?search=' + searchParam;
+    var searchInput = document.getElementById('searchInput');
+    searchInput.value = searchParam;
+    var titleElemtn = document.getElementById('products-title');
+    titleElemtn.innerHTML = 'نتایج جستجو برای ' + searchParam;
+}
+
 if (previousPage > 0) {
-    leftBtn.href = './' + lastPath + '?Page=' + previousPage;
+    href += '&Page=' + previousPage;
+    
+    leftBtn.href = href;
 }
 if (nextPage <= maxPage) {
-    rightBtn.href = './' + lastPath + '?Page=' + nextPage;
+    href += '&Page=' + nextPage;
+    rightBtn.href = href;
 }
